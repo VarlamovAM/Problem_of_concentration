135abb110301a714ec3fe12c1c47ef1bf19f182d
Varlamov.AM <varlamovantonij@gmail.com>
2020-06-20 00:52:44 +0300

    New file
    File with main function of project. First initial commit.

---
+++ main.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+
+int main(){
+	/* Main function of project. in this function we will count parametrs
+	 * of solution from inputs data from user.
+	 * In input we will get starts solution param. and final solution param.
+	 * In output we will write param. of buffer solution*/
+		
+
+	return 0;
+}
