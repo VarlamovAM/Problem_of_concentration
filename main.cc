9dc4e900bd73800d6fc89ec03d8d59880630dcf6
Varlamov.AM <varlamovantonij@gmail.com>
2020-06-22 02:37:56 +0300

    New version
    In this version of the program, a function has been added that allows you to
    obtain experimental data on the dependence of acid density on solution density.
    This data placed in file "Sulfur_acid.txt"

--- main.cc
+++ main.cc
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <fstream>
 
 
 
 int const Size = 100;
 
+
+struct Acid_data{
+    double rho[100];
+    double concentration[100];
+
+};
 
 
 void Print_Matrix(float* A){
@@ -50,6 +57,7 @@ void Matrix_string_convertation(float* A, int pos_base_string, int pos_chaning_s
     return;
 }
 
+
 void Matrix_convertation(float* A, int i, int j){
 
 
@@ -71,32 +79,86 @@ void Matrix_convertation(float* A, int i, int j){
     return;
 }
 
+
+double Power(double x, int pow){
+
+    double results = x;
+    int i = 2;
+
+    while (i <= pow){
+        results = results * x;
+        i++;
+    }
+    i = 2;
+
+
+    return results;
+}
+
+
+double Summ(double* A, int number_of_terms){
+
+    double results = 0;
+    int i = 0;
+
+
+    while(i <= number_of_terms){
+        results += *(A + i);
+        i++;
+    }
+
+    return results;
+}
+
+
+struct Acid_data Get_data(){
+
+    struct Acid_data Sulfur_acid_data;
+
+
+    int N = 0;
+    int h = 0;
+
+    std::string line;
+
+    std::ifstream in("C:\Sulfur_acid.txt"); // окрываем файл для чтения
+    if (in.is_open())
+    {
+        in >> N;
+
+
+        for ( h >= 0; h < N; h++){
+            double a = 0.;
+            double b = 0.;
+            in  >> a >> b;
+            Sulfur_acid_data.rho[h] = a;
+            Sulfur_acid_data.concentration[h] = b;
+
+        }
+
+        h = 0;
+
+    }
+
+    in.close();     // закрываем файл
+
+
+    return Sulfur_acid_data;
+};
+
+
 int main(){
 	/* Main function of project. in this function we will count parametrs
 	 * of solution from inputs data from user.
 	 * In input we will get starts solution param. and final solution param.
 	 * In output we will write param. of buffer solution*/
 
-    float A[Size][Size];
+    double A[Size][Size];
 
-    int i = 0;
-    int j = 0;
-
-    for(i >= 0; i < Size; i++){
-        for (j >= 0; j < Size; j++){
-            A[i][j] = Size * i + j + 1;
-        }
-        j = 0;
-    }
+    struct Acid_data Sulfur_acid_data = Get_data();
 
 
-    i = 0;
-    j = 0;
-
-    Matrix_convertation(&A[0][0], i, j);
-    std::cout << "final";
-
-
+    std::cout << Sulfur_acid_data.concentration[80];
 
 	return 0;
 }
